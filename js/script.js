2f68925e2161a9b172484b549d65c3e20799eca2
Zakrieev <57872517+Zakrieev@users.noreply.github.com>
2020-02-23 21:14:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,51 @@
-const charactersUrl = "https://rickandmortyapi.com/api/character/";
+const baseUrl = "https://rickandmortyapi.com/api/";
+const Urlcharacter = `${baseUrl}character/`;
 
-fetch(baseUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(json) {
-        caracthers(json);
-    })
-    .catch(function() {
-        document.location.href = "error.html";
-    });
+window.addEventListener("load", event => {
+  console.log("page is fully loaded");
+});
 
-
-function characters(json) {
-
-    const results = json.results;
-    console.dir(results);
-    
-    const loader = document.querySelector(".loader");
-    loader.style.display = "none";
-
-    const resultsContainer = document.querySelector(".results");
-    let html = "";
-
-    results.forEach(function(result) {
-    
-        let unknownType = "Unknown"; 
-
-        if(result.type) {
-            unknownType = result.type;
-        }
-        html += `<div class="col-sm-6 col-md-4 col-lg-3">                
-                    <div class="card">
-                        <img class="image" src="${result.image}" alt="${result.name}">
-                        <div class="details">
-                            <h4 class="name">${result.name}</h4>
-                            <p>Type: ${unknownType}</p>    
-                            <p>Episode count: ${result.episode.length}</p>                                       
-                            <a class="btn btn-primary" href="details.html?id=${result.id}">Details</a>
-                        </div>
-                    </div>
-                </div>`
-        
-    });
-
-resultsContainer.innerHTML = html;
-
+function rickAndMorty(response) {
+  return response.json();
 }
+
+function handleCharacters(json) {
+  const characterLoad = json.results;
+  console.dir(json);
+
+  const resultsContainer = document.querySelector(".results");
+
+  let html = "";
+
+  characterLoad.forEach(function(result) {
+    let typeOf = result.type;
+    if (result.type.length > 0) {
+    } else {
+      typeOf = "Unknown";
+    }
+
+    html += `<div class="col-sm-6 col-md-4 col-lg-3">                
+      <div class="card">
+          <img class="image" src="${result.image}" alt=${result.name}>
+          <div class="details">
+              <h4 class="name">${result.name}</h4>
+              <p>Type: ${typeOf}</p>
+              <p>Episode count: ${result.episode.length}</p>                                       
+              <a class="btn btn-primary" href="details.html?id=${result.id}">Details</a>
+          </div>
+      </div>
+  </div>`;
+  });
+
+  resultsContainer.innerHTML = html;
+}
+
+function ifError(error) {
+  console.log(error);
+}
+
+fetch(Urlcharacter)
+  .then(rickAndMorty)
+  .then(handleCharacters)
+  .catch(ifError);
+    
