10644afe09f81918111ce17bf68f77c624b0f6a5
Zakrieev <57872517+Zakrieev@users.noreply.github.com>
2020-02-23 21:35:55 +0100

    Update details.js

--- js/details.js
+++ js/details.js
@@ -1,50 +1,48 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 
-let id;
-
-if(params.has("id")) {
-    id = params.get("id");
+if (params.has("id")) {
+  id = params.get("id");
 } else {
-    document.location.href = "/";
+  document.location.href = "/";
 }
 
-const baseURL = "https://rickandmortyapi.com/api/";
-const charURL = `${baseURL}character/`;
-const DetailURL = `${characterURL}${id}`;
+const baseUrl = "https://rickandmortyapi.com/api/";
+const characterUrl = `${baseUrl}character/`;
+const idUrl = `${characterUrl}${id}`;
 
-fetch(DetailURL)
+fetch(idUrl)
   .then(function(response) {
     return response.json();
   })
   .then(function(json) {
-    createDetails(json);
+    Details(json);
   })
   .catch(function(error) {
     console.dir(error);
   });
 
-function characterDetails(json) {
+function Details(json) {
   console.dir(json);
-	
+
+  const name = document.querySelector("h1");
+  name.innerHTML = json.name;
+
+  const status = document.getElementById("status");
+  status.innerHTML = json.status;
+
+  const species = document.getElementById("species");
+  species.innerHTML = json.species;
+
+  const origin = document.getElementById("origin");
+  origin.innerHTML = json.origin.name;
+
+  const location = document.getElementById("location");
+  location.innerHTML = json.location.name;
+
   const image = document.querySelector(".details-image");
   image.src = json.image;
   image.alt = json.name;
 
   document.title = json.name + " | " + document.title;
-
-  const name = document.querySelector("h1");
-  name.innerHTML = json.name;
-
-  const status = document.querySelector("status");
-  status.innerHTML = json.status;
-
-  const species = document.querySelector("species");
-  species.innerHTML = json.species;
-
-  const origin = document.querySelector("origin");
-  origin.innerHTML = json.origin.name;
-
-  const location = document.querySelector("location");
-  location.innerHTML = json.location.name;
 }
