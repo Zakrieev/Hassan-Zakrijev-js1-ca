70096b325028607bd009c099d42090affaa55f3e
Zakrieev <57872517+Zakrieev@users.noreply.github.com>
2020-02-23 18:02:01 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,25 @@
 const form = document.querySelector("#contactForm");
-form.addEventListener("submit", validateForm);
+form.addEventListener("submit", checkform);
+
+function checkform(event) {
+  event.preventDefault();
+
+  const firstName = document.querySelector("#firstName");
+  const firstNameError = document.querySelector("#firstNameError");
+  const firstNameValue = firstName.value;
+
+  if (checkInputLength(firstNameValue) === true) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+  }
+  
+  const lastName = document.querySelector("#lastName");
+  const lastNameError = document.querySelector("#lastNameError");
+  const lastNameValue = lastName.value;
+
+  if (checkInputLength(lastNameValue) === true) {
+    lastNameError.style.display = "none";
+  } else {
+    lastNameError.style.display = "block";
+  }
