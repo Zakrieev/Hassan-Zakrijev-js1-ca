0468d7e865e226c71622d6d32726a09157c6586b
Zakrieev <57872517+Zakrieev@users.noreply.github.com>
2020-02-23 18:04:37 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -23,3 +23,56 @@ function checkform(event) {
   } else {
     lastNameError.style.display = "block";
   }
+
+  const email = document.querySelector("#email");
+  const emailError = document.querySelector("#emailError");
+  const invalidEmailError = document.querySelector("#invalidEmailError");
+
+  const emailValue = email.value;
+
+  function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    return regEx.test(email);
+  }
+
+  if (checkInputLength(emailValue) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (validateEmail(emailValue) === true) {
+    invalidEmailError.style.display = "none";
+  } else {
+    invalidEmailError.style.display = "block";
+  }
+
+  function checkInputLength(value) {
+    const trimmedValue = value.trim();
+
+    if (trimmedValue.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const message = document.querySelector("#message");
+  const messageError = document.querySelector("#messageError");
+  const messageValue = message.value;
+
+  if (checkInputLength(messageValue) === true) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+  function checkInputLength(value) {
+    const trimmedValue = value.trim();
+
+    if (trimmedValue.length > 9) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
