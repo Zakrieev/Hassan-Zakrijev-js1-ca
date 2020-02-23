744a88ef40dd25801aa99e1a962b970ecd3f986e
Zakrieev <57872517+Zakrieev@users.noreply.github.com>
2020-02-23 17:29:00 +0100

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
-function updateContent() {
+function updatetext() {
   document.body.innerHTML = document.body.innerHTML.replace(/The/g, "Replaced");
   document.body.innerHTML = document.body.innerHTML.replace(/the/g, "replaced");
 }
